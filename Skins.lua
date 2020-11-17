617fb7ac32065de1545dbf03f3f903354b7e9696
StormFX <stormfx@gmail.com>
2020-11-17 14:41:20 -0600

    - Remove third parameter.

--- Skins.lua
+++ Skins.lua
@@ -488,7 +488,7 @@ MSQ:AddSkin("Cirque", {
 		OffsetY = 0,
 		-- SetAllPoints = true,
 	},
-}, true)
+})
 
 ----------------------------------------
 -- Cirque - Simple
@@ -537,4 +537,4 @@ MSQ:AddSkin("Cirque - Simple", {
 		-- UseStates = nil,
 		-- SetAllPoints = nil,
 	},
-}, true)
+})
